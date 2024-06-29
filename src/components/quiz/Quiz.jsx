d06d54c6ed789a93fd4c0643be841576b8c027ca
shakil88818 <shakil88818@gmail.com>
2024-06-30 01:57:59 +0600

    design part updeted

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -2,9 +2,21 @@ import "./Quiz.css"
 
 const Quiz = () => {
   return (
-    <div>
-      quiz
-    </div>
+    <>
+      <main className="container">
+        <h2>Quiz Application</h2>
+        <hr />
+        <h3>1. What does CSS stand for?</h3>
+        <ul>
+            <li>Creative Style Sheets</li>
+            <li>Cascading Style Sheets</li>
+            <li>Colour Style Sheets</li>
+            <li>Computer Style Sheets</li>
+        </ul>
+        <h5 className="button">Next</h5>
+        <p className="details">1 of 5 questions</p>
+      </main>
+    </>
   )
 }
 
