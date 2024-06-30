9ff1de0cebf4a28bdbbd09349059fccc15b36aed
shakil88818 <shakil88818@gmail.com>
2024-06-30 09:22:14 +0600

    functionality part updated

---
+++ src/assets/Data.js
@@ -0,0 +1,30 @@
+export const Data = [
+    {
+        question: "What kind of noun is 'Cattle'?",
+        option1:"proper",
+        option2:"common",
+        option3:"collective",
+        option4:"material",
+        ans:3
+    },
+
+    {
+        question: "What kind of noun is 'Girl'",
+        option1:"proper noun",
+        option2:"common noun",
+        option3:"collective noun",
+        option4:"material noun",
+        ans:2
+    },
+
+    {
+        question: "Which is the noun of the word 'beautiful'?",
+        option1:"Beauty",
+        option2:"Beautify",
+        option3:"Beauteous",
+        option4:"Beautifully",
+        ans:1
+    }
+
+
+]
--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,20 +1,88 @@
+
+import { useRef, useState } from "react"
 import "./Quiz.css"
+import { Data } from "../../assets/Data"
 
 const Quiz = () => {
+
+  let [index, setIndex] = useState(0)
+  let [question, setQuestion] = useState(Data[index])
+  let [lock, setLock] = useState(false)
+  let [score, setScore] = useState(0)
+  let [result, setResult] = useState(false)
+
+  const option1 = useRef(null)
+  const option2 = useRef(null)
+  const option3 = useRef(null)
+  const option4 = useRef(null)
+
+  const option_array = [option1, option2, option3, option4]
+
+  const checkAns = (e,ans)=>{
+    if(lock === false){
+      if(question.ans === ans){
+        e.target.classList.add("correct")
+        setLock(true)
+        setScore((prev)=>prev+1)
+      }else{
+        e.target.classList.add("wrong")
+        setLock(true)
+        option_array[question.ans-1].current.classList.add("correct")
+      }
+    }
+  }
+
+  const next=()=>{
+    if(lock === true){
+      if(index === Data.length-1){
+        setResult(true)
+        return 0;
+      }
+    }
+    if(lock === true){
+      setIndex(++index)
+      setQuestion(Data[index])
+      setLock(false)
+      option_array.map((option)=>{
+        option.current.classList.remove("wrong")
+        option.current.classList.remove("correct")
+      })
+      return null;
+    }
+  }
+
+
+  const reset = ()=>{
+    setIndex(0)
+    setQuestion(Data[0])
+    setScore(0)
+    setLock(false)
+    setResult(false)
+  }
   return (
     <>
       <main className="container">
-        <h2>Quiz Application</h2>
+        <div className="top-content">
+          <h2>Quiz Application</h2>
+          <p>{index+1} of {Data.length} questions</p>
+        </div>
         <hr />
-        <h3>1. What does CSS stand for?</h3>
-        <ul>
-            <li>Creative Style Sheets</li>
-            <li>Cascading Style Sheets</li>
-            <li>Colour Style Sheets</li>
-            <li>Computer Style Sheets</li>
-        </ul>
-        <h5 className="button">Next</h5>
-        <p className="details">1 of 5 questions</p>
+        {result? <></> : <>
+          <h3>{index+1}. {question.question}</h3>
+          <ul>
+              <li ref={option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+              <li ref={option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+              <li ref={option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+              <li ref={option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+          </ul> 
+          <h5 onClick={next} className="button">Next</h5>
+          
+        </>}
+        
+        {result? <>
+          <h3 style={{textAlign: "center"}}>You Scored {score} Out of {Data.length}</h3>
+          <button onClick={reset} className="button">Reset</button>
+        </>: <></>}
       </main>
     </>
   )
